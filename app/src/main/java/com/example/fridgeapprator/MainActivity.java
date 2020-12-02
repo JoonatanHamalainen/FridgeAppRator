fd14cace41e2e89369cf236312e34b7c639a6122
Joonatan Hämäläinen <joonatan.hamalainen@metropolia.fi>
2020-12-02 16:08:13 +0200

    added starting screen fragment and made changes to shopping list fragment. Also added two new classes WelcomePageFragment and ShoppingListFragment

--- app/src/main/java/com/example/fridgeapprator/MainActivity.java
+++ app/src/main/java/com/example/fridgeapprator/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.fridgeapprator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.example.fridgeapprator.fragment.ShoppingListFragment;
+import com.example.fridgeapprator.fragment.WelcomePageFragment;
 import com.example.fridgeapprator.model.Product;
 import com.example.fridgeapprator.model.ProductType;
 import com.example.fridgeapprator.model.ShoppingList;
@@ -25,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     ProductViewModel productViewModel;
     ShoppingListProductViewModel shoppingListProductViewModel;
     ShoppingListViewModel shoppingListViewModel;
+    ShoppingListFragment shoppingListFragment;
+    WelcomePageFragment welcomePageFragment;
 
 
     @Override
@@ -52,5 +60,57 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+            // create Calculator-fragment programmatically...
+            shoppingListFragment = new ShoppingListFragment();
+            welcomePageFragment = new WelcomePageFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            transaction.add(R.id.fragment_container, welcomePageFragment, "welcomePageFragment");
+            //transaction.addToBackStack(null); // to get back from ListFragment
+            // Commit the transaction
+            transaction.commit();
+        }
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        /*if (id == R.id.menu_shoppingList) {
+            //FragmentManager fragmentManager = getSupportFragmentManager();
+            Fragment fragment = getSupportFragmentManager().findFragmentById(R.layout.shopping_list_fragment);
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            //transaction.remove(fragment).commit(); // wipe the old
+            ShoppingListFragment shoppingListFragment = new ShoppingListFragment();
+            transaction.addToBackStack(null);
+            transaction.replace(R.id.fragment_container, shoppingListFragment)
+                    .commit();
+        }*/
+        return true;
+    }
+
+
 }
---
+++ app/src/main/java/com/example/fridgeapprator/fragment/ShoppingListFragment.java
@@ -0,0 +1,22 @@
+package com.example.fridgeapprator.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.fridgeapprator.R;
+
+public class ShoppingListFragment extends Fragment {
+
+    public ShoppingListFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.shopping_list_fragment, container, false);
+        return v;
+    }
+}
