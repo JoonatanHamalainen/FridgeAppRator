1f34e5a21422ab40a5ed7390fc560e6e0b290829
Joonatan Hämäläinen <joonatan.hamalainen@metropolia.fi>
2020-12-14 16:36:51 +0200

    added instructions.html and html-files that open from it. Links not working jet..

--- app/src/main/java/com/example/fridgeapprator/fragment/InstructionsFragment.java
+++ app/src/main/java/com/example/fridgeapprator/fragment/InstructionsFragment.java
@@ -1,10 +1,18 @@
 package com.example.fridgeapprator.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
 
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import com.example.fridgeapprator.R;
@@ -13,8 +21,18 @@ public class InstructionsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        super.onCreate(savedInstanceState);
 
         View view = inflater.inflate(R.layout.instructions_fragment, container, false);
+
+        WebView web = (WebView)view.findViewById(R.id.webViewInstructions);
+        web.loadUrl("file:///android_asset/instructions.html");
+
+        TextView t2 = (TextView) view.findViewById(R.id.instructionFridge);
+        t2.setMovementMethod(LinkMovementMethod.getInstance());
 
         return view;
 
